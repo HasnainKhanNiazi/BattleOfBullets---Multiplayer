aa8f5743f6b767c2e5a91242995f00f954eddb2c
HasnainKhanNiazi <hasikhan144@gmail.com>
2018-03-17 17:27:49 +0500

    Updated gitignore without Photon Models

--- Assets/Scripts/PlayerScoreSaver.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerScoreSaver : MonoBehaviour {
-	public int SaveKills;
-	public int SaveDeaths;
-}
